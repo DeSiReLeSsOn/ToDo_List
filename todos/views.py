97b4c9f8f13a270880253ecc54a22890813b1a3e
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-05-11 05:06:52 +0300

    added basic_method for additional todo

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 from .models import Todo
 from .serializers import TodoSerializer
 
@@ -11,4 +14,21 @@ class ListTodo(generics.ListAPIView):
 
 class  DetailTodo(generics.RetrieveAPIView):
     queryset = Todo.objects.all() 
-    serializer_class = TodoSerializer
+    serializer_class = TodoSerializer 
+
+
+
+class CreateTodo(APIView):
+
+    def get(self, request):
+        todo = Todo.objects.all() 
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)  
+
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.errors, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
