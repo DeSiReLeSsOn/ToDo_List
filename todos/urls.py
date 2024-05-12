1eb88ce9c25040e203757c05dfa86e8177d0c0b8
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-05-12 05:34:37 +0300

    add simple router for a new viewset

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-from .views import ListTodo, DetailTodo, CreateTodo
+from django.urls import path, include
+from .views import ListTodo, DetailTodo, CRUDTodo
+from rest_framework import routers  
+
+router = routers.SimpleRouter()
+router.register(r'todo', CRUDTodo)
+
 
 urlpatterns = [
-    path('<int:pk>/', DetailTodo.as_view()),
-    path('', ListTodo.as_view()),
-    path('create/', CreateTodo.as_view())
+    # path('<int:pk>/', DetailTodo.as_view()),
+    # path('', ListTodo.as_view()),
+    # path('create/', CRUDTodo.as_view()),
+    path('', include(router.urls)),
 ]
