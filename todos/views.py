a18be5b03fa73385d3a171946693ba51e8897c1b
DeSiReLeSsOn <theproblemdi@gmail.com>
2024-05-12 05:33:54 +0300

    create viewset for todo

--- todos/views.py
+++ todos/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from .models import Todo
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer 
+from rest_framework import viewsets
 
 
 class ListTodo(generics.ListAPIView):
@@ -18,17 +18,22 @@ class  DetailTodo(generics.RetrieveAPIView):
 
 
 
-class CreateTodo(APIView):
+# class CreateTodo(APIView):
 
-    def get(self, request):
-        todo = Todo.objects.all() 
-        serializer = TodoSerializer(todo, many=True)
-        return Response(serializer.data)  
+#     def get(self, request):
+#         todo = Todo.objects.all() 
+#         serializer = TodoSerializer(todo, many=True)
+#         return Response(serializer.data)  
 
-    def post(self, request):
-        serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.errors, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = TodoSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.errors, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+
+
+class CRUDTodo(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
